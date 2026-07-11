e2ce8b154f4c527cf9c628ee7d3178c0ab72001c
mandil1992 <mandil.malla@l-c-style.co.jp>
2026-07-12 00:16:43 +0900

    Update message in GetMessage function call

--- 6.cs
+++ 6.cs
@@ -8,7 +8,7 @@ class Program
 
         Console.WriteLine($"Sum: {result}");
 
-        string message = GetMessage("Mandil");
+        string message = GetMessage("Mandil is tryign to solve this.");
         Console.WriteLine(message);
     }
 
