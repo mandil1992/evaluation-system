c7961c2e275788f397f7eeb5c927f94a8321e808
mandil1992 <mandil.malla@l-c-style.co.jp>
2026-07-12 00:29:05 +0900

    test -message

--- 7.cs
+++ 7.cs
@@ -8,7 +8,7 @@ class Program
 
         Console.WriteLine($"Sum: {result}");
 
-        string message = GetMessage("Mandil");
+        string message = GetMessage("Mandil oo my god");
         Console.WriteLine(message);
     }
 
